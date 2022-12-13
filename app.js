e52cd140b41cc636b29c3ddd9c946db314adbdbb
SIMON BULLADO <s_bullado51@hotmail.com>
2022-12-13 01:45:35 +0100

    Create route user

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+const port = process.env.APP_PORT ?? 5000;
+
 const express = require("express");
 
 const app = express();
 
-const port = process.env.APP_PORT ?? 5000;
-const welcome = (req, res) => {
+function welcome(req, res) {
   res.send("Welcome to my favourite movie list");
-};
+}
 
 app.get("/", welcome);
 
@@ -13,6 +15,11 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersId);
 
 app.listen(port, (err) => {
   if (err) {
